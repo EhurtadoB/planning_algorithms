f919594b0269a778645340903b20907ed12308bf
jpuentevel <jpuentevelasquez@gmail.com>
2022-04-13 23:21:51 -0500

    Orden corregido

--- SimuladorProcesos/src/logica/Algoritmo.java
+++ SimuladorProcesos/src/logica/Algoritmo.java
@@ -1,5 +1,6 @@
 package logica;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,11 +9,25 @@ public class Algoritmo {
     public void ordenar(Proceso[] procesos) {
         for(int i = 0; i < procesos.length; i++) {
             for(int j = 0; j < procesos.length - 1; j++) {
-                float auxActual = procesos[j].t_llegada;
-                float auxSig = procesos[j+1].t_llegada;
-                if(auxActual > auxSig) {
-                    procesos[j].t_llegada = auxSig;
-                    procesos[j+1].t_llegada = auxActual;
+
+                float auxllegadaActual = procesos[j].t_llegada;
+                float auxllegadaSig = procesos[j+1].t_llegada;
+                String auxNomActual = procesos[j].nomProceso;
+                String auxNomSig = procesos[j+1].nomProceso;
+                float auxPrioridadActual = procesos[j].prioridad;
+                float auxPrioridadSig = procesos[j+1].prioridad;
+                float auxCpuActual = procesos[j].t_cpu;
+                float auxCpuSig = procesos[j+1].t_cpu;
+
+                if(auxllegadaActual > auxllegadaSig) {
+                    procesos[j].t_llegada = auxllegadaSig;
+                    procesos[j+1].t_llegada = auxllegadaActual;
+                    procesos[j].nomProceso = auxNomSig;
+                    procesos[j+1].nomProceso = auxNomActual;
+                    procesos[j].prioridad = auxPrioridadSig;
+                    procesos[j+1].prioridad = auxPrioridadActual;
+                    procesos[j].t_cpu = auxCpuSig;
+                    procesos[j+1].t_cpu = auxCpuActual;
                 }
             }
         }
